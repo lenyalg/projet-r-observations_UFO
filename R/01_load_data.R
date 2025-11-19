c28e12b0e765c274abcd6d9f6d9b02fe100e8d1e
lenyalg <alguazilleny@gmail.com>
2025-11-19 15:36:53 +0100

    Update 01_load_data.R

--- R/01_load_data.R
+++ R/01_load_data.R
@@ -1,17 +1,9 @@
 # ===================================================================
 # 01_LOAD_DATA.R
-# Charge les données brutes depuis /data
 # ===================================================================
 
-library(readr) # readr::read_csv est plus rapide que read.csv
-
-# Chemin relatif vers le fichier de données
+library(readr) 
 data_path <- "data/complete.csv"
-
-# Charge les données dans un objet 'raw_data'
-# On gère les problèmes de type ici si nécessaire
 raw_data <- readr::read_csv(data_path, 
-                            col_types = cols(.default = "c")) # Charger tout en 'character' d'abord est plus sûr
-
-# Message pour la console (bon pour le debug)
+                            col_types = cols(.default = "c")) 
 message("Données brutes ('raw_data') chargées.")
